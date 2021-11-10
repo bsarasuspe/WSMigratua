f3ae7bd73538f2cab3eea6703dc8fb9f5889689a
bsarasuspe <83415989+bsarasuspe@users.noreply.github.com>
2021-11-10 17:11:13 +0100

    Derrigorrezkoa egina

--- php/AddVip.php
+++ php/AddVip.php
@@ -17,16 +17,20 @@
                 <!-- Eposta igorri -->
                 <input type="submit" name="submit" id="submit" value="VIPa bihurtu"><br>
             </form><br>
-
 <?php
     if (isset($_POST['eposta'])){
-        $curl = curl_init();
-        $eposta = $_POST['eposta'];
-        $url = "http://localhost/WSMIG/WSMigratua/rest/VipUsers/".$eposta;
-        curl_setopt($curl, CURLOPT_URL, $url);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-        $str = curl_exec($curl);
-        echo $str; 
+      $ch = curl_init();
+      $eposta = $_POST['eposta'];
+      curl_setopt($ch, CURLOPT_URL, "http://localhost/WSMIG/WSMigratua/rest/VipUsers/");
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+      curl_setopt($ch, CURLOPT_POST, true);
+      $data = array(
+        'eposta'=> $eposta,
+      );
+      curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+      $output = curl_exec($ch);
+      echo $output;
+      curl_close($ch);
     }
 ?>
     </div>
