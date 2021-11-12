349b6f111054ee13aea8511cf668ba5fd677cf4e
bsarasuspe <83415989+bsarasuspe@users.noreply.github.com>
2021-11-12 18:53:52 +0100

    Delete

---
+++ php/DeleteVip.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <?php include '../html/Head.html'?>
+</head>
+<body>
+  <?php include '../php/Menus.php' ?>
+  <section class="main" id="s1">
+    <div>
+
+      <h3>Ezabatu VIP erabiltzailea</h3><br>
+      <form id="form" name="form" method="post">
+                <!-- Eposta -->
+                <label for="eposta">Eposta (*):</label>
+                <input type="text" id="eposta" name="eposta"><br><br>
+
+                <!-- Eposta igorri -->
+                <input type="submit" name="submit" id="submit" value="VIPa bihurtu"><br>
+            </form><br>
+
+      <?php
+      if (isset($_POST['eposta'])){
+        $ch = curl_init();
+        $eposta = $_POST['eposta'];
+        $url = "http://localhost/WSMIG/WSMigratua/rest/VipUsers/".$eposta;
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'DELETE');
+        $output = curl_exec($ch);
+        echo $output;
+        curl_close($ch);
+        }
+      ?>
+
+    </div>
+  </section>
+  <?php include '../html/Footer.html' ?>
+</body>
+</html>
